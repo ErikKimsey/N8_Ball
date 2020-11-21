817eca1e1c262c44e8ba4b709d229db12367636e
Erik <kimseyerik@gmail.com>
2020-11-21 12:20:30 -0500

    Created new model for pyramid, new emmissive material for pyramid, added reverse gravity script

---
+++ Assets/Scripts/PyramindShakeNGravity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PyramindShakeNGravity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody rb;
+    public float y;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Debug.Log(rb);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        y += Time.deltaTime;
+        rb.AddForce(0f,y, 1f);
+    }
+}
